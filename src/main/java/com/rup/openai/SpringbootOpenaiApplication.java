ed387dcf828ae0e8a6cdccd80fcc899712228e0e
Rupendra Raghu <rupendraraghu@gmail.com>
2024-06-12 17:53:52 +0530

    first commit

---
+++ src/main/java/com/rup/openai/SpringbootOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.rup.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rup/openai/SpringbootOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rup.openai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
